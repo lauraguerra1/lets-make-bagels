4037a17299bb340ecd24f7683955ba35c3b81614
Laura Guerra <l.garciaguerra1@gmail.com>
2023-04-07 18:31:14 -0400

    Reconstruct page to accept integer input

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-let recipeEl = document.getElementById("recipe-el");
-let countEl = document.getElementById("count-el");
-let count = 0;
-// let list = document.getElementById("myList");
+// var recipeEl = document.getElementById("recipe-el");
+var flourEl = document.querySelector('.flour');
+var bakingSoda = document.querySelector('.baking-soda');
+var bakingPowder = document.querySelector('.baking-powder');
+var yogurt = document.querySelector('.yogurt');
+var salt = document.querySelector('.salt');
+var count = document.getElementById('bagel-amt');
+var countEl = document.querySelector('.count-number')
 
-let recipe = {
+var recipe = {
   flour: 60,
   yogurt: 82.5,
   bakingSoda: 0.125,
@@ -11,29 +15,31 @@ let recipe = {
   salt: 0.125,
 };
 
-// let macros = {
+// var macros = {
 //     calories: 202,
 //     protein: 42,
 //     netCarbs: 11.4,
 //     fat: 2, 
 // }
 function changeRecipe(count) {
-  if (count) {
-    let newRecipe = {
-      flour: (recipe.flour * count),
-      yogurt: (recipe.yogurt * count),
-      bakingSoda: (recipe.bakingSoda * count),
-      bakingPowder: (recipe.bakingPowder * count),
-      salt: (recipe.salt * count),
+  // if (count) {
+    var countNum = parseInt(count)
+    var newRecipe = {
+      flour: (recipe.flour * countNum),
+      yogurt: (recipe.yogurt * countNum),
+      bakingSoda: (recipe.bakingSoda * countNum),
+      bakingPowder: (recipe.bakingPowder * countNum),
+      salt: (recipe.salt * countNum),
     };
-    return `To make ${count} bagel(s), you will need:
-     ${newRecipe.flour}g flour, ${newRecipe.yogurt}g yogurt, ${newRecipe.bakingPowder} tsp baking powder, ${newRecipe.bakingSoda} tsp baking soda, ${newRecipe.salt} tsp salt.`;
-  } else {
-    return `Are you sure you don't want to make any bagels? That's sad.`
-  }
+    return newRecipe;
+  //   return `To make ${count} bagel(s), you will need:
+  //    ${newRecipe.flour}g flour, ${newRecipe.yogurt}g yogurt, ${newRecipe.bakingPowder} tsp baking powder, ${newRecipe.bakingSoda} tsp baking soda, ${newRecipe.salt} tsp salt.`;
+  // } else {
+  //   return `Are you sure you don't want to make any bagels? That's sad.`
+  // }
 }
 // function changeMacros(count) {
-//     let newMacros = {
+//     var newMacros = {
 //         calories: (macros.calories * count),
 //         protein: (macros.protein * count),
 //         netCarbs: (macros.netCarbs * count),
@@ -41,16 +47,20 @@ function changeRecipe(count) {
 //       };
 //       return `${newMacros.calories} calories, `
 // }
-function increment() {
-  count += 1;
-  countEl.textContent = count;
-}
+// function increment() {
+//   count += 1;
+//   countEl.textContent = count;
+// }
 
 function newRecipe() {
-  recipeEl.textContent = changeRecipe(count)
-  countEl.textContent = 0;
-  count = 0;
+  console.log(count.value)
+  var newRecipe = changeRecipe(count.value);
+  countEl.innerText = `${count.value}`
+  flourEl.innerText = `${newRecipe.flour}`;
+  yogurt.innerText = `${newRecipe.yogurt}`;
+  bakingPowder.innerText = `${newRecipe.bakingPowder}`;
+  bakingSoda.innerText = `${newRecipe.bakingSoda}`;
+  salt.innerText = `${newRecipe.salt}`;
 }
-
 
 
